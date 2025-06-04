6a872844bae43184aa01533b7bedee99f385de33
Sebastian Lema <sebasjhonlemamontoya12@gmail.com>
2025-06-04 15:59:14 -0500

    Se importo Flask y se añadio la variable para cargar el entorno desde el archivo .env

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ Script principal para ejecutar la aplicación Flask.
 import os
 from musica_api import create_app
 from dotenv import load_dotenv
+from flask import request
 
 # TODO: Cargar variables de entorno desde archivo .env si existe
+load_dotenv()
 
 # TODO: crear la aplicación
+app = create_app()
 
 if __name__ == "__main__":
+    
     # TODO: Obtener puerto del ambiente o usar 5000 por defecto
+    port = int(os.getenv("PORT", 5000))
     
     # TODO: Determinar si se debe usar modo debug
+    debug = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
     
     # TODO: Ejecutar aplicación
-
-    pass
+    app.run(host='0.0.0.0', port=port, debug=debug)
